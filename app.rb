2e66eb520f4eef895b248ba25019dd8f08ea9704
shaygyawali <shaygyawali@gmail.com>
2018-06-27 15:49:44 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,11 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "Welcome to your app!!!! Hbndkijdf "
+  end
+  
+  get '/info' do
+    "Sinatra is cool!"
   end
 
 end
